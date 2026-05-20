bd1bbbeef190d556fd49af6acc1ea9b1ff25d677
Nicolas <nicolasrodriguezcabrera16@gmail.com>
2026-05-20 18:45:50 -0500

    Primer commit

---
+++ scripts.js
@@ -0,0 +1,36 @@
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+
+/* BOTON 1 */
+btn1.addEventListener("click", () => {
+
+    document.body.style.backgroundImage =
+    "url('IMAGEN/BETTA.jpg')";
+
+});
+
+/* BOTON 2 */
+btn2.addEventListener("click", () => {
+
+    document.body.style.backgroundImage =
+    "url('IMAGEN/DISCO IM.jpg')";
+
+});
+
+/* BOTON 3 */
+btn3.addEventListener("click", () => {
+
+    document.body.style.backgroundImage =
+    "url('IMAGEN/MAR.jpg')";
+
+});
+
+/* BOTON 4 */
+btn4.addEventListener("click", () => {
+
+    document.body.style.backgroundImage =
+    "url('IMAGEN/RAYA.jpg')";
+
+});
